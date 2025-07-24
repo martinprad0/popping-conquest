e938d1af7fc4b6e217f550d071f86720dd2f2c8b
martin <martinpradoguerra@gmail.com>
2025-07-24 07:42:32 -0500

    yes yes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -72,7 +72,7 @@
         <div
             class="flex w-[90%] mx-auto justify-center mb-6 bg-white/70 shadow-md px-6 py-4 border-0 border-primary-200 rounded"
         >
-            <h2 class="text-2xl">Cronograma</h2>
+            <h1>Cronograma</h1>
         </div>
         <div class="flex justify-center">
             <Table
@@ -104,7 +104,7 @@
         <div
             class="flex w-[90%] mx-auto justify-center mb-6 bg-white/70 shadow-md px-6 py-4 border-0 border-primary-200 rounded"
         >
-            <h2 class="text-2xl">Cómo llegar</h2>
+            <h1>Cómo llegar</h1>
         </div>
 
         <div class="flex justify-center">
